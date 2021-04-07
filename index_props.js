ad19e6ba918420cd5c579618850e7f71b56cfbd9
eason_tseng <eason_tseng@mail.chungyo.net>
2021-04-07 14:05:43 +0800

    測試

--- index_props.js
+++ index_props.js
@@ -4,24 +4,38 @@ export default {
     'pagePath': "README.md",
     'layoutPath': "_layout.tsx",
     'outputPath': "index.html",
-    'title': "Hello world effewefw",
+    'title': "The Dream-Eaters",
     'content': React.createElement("article", { dangerouslySetInnerHTML: {
-            __html: '<h1>Hello world effewefw</h1>\n<p>123</p>'
+            __html: '<h1>The Dream-Eaters</h1>\n<h2 id="dreamlands">Dreamlands：<a class="anchor" href="#dreamlands">§</a></h2>\n<p><img src="https://arkhamdb.com/bundles/cards/05001.png" alt="Carolyn Fern"></p>\n<p>Packs: From Core Set (2) to Before the Black Throne</p>\n<h3 id="assets">Assets<a class="anchor" href="#assets">§</a></h3>\n<ul>\n<li>2x Clarity of Mind [3] (Return to the Dunwich Legacy)</li>\n<li>1x Scavenging (Core Set)</li>\n<li>2x Liquid Courage (The Dunwich Legacy)</li>\n<li>2x Peter Sylvestre:Big Man on Campus [2] (The Dunwich Legacy)</li>\n<li>1x Stick to the Plan [3] (Black Stars Rise)</li>\n<li>2x St. Hubert\'s Key:Cleansing Fire (Black Stars Rise)</li>\n<li>1x Hypnotic Therapy (The Circle Undone)</li>\n<li>2x Fingerprint Kit (The Circle Undone)</li>\n<li>1x Four of Cups:Chalice of the Heart [1] (The Circle Undone)</li>\n<li>2x Hawk-Eye Folding Camera (The Wages of Sin)</li>\n<li>1x Hallowed Mirror (Before the Black Throne)</li>\n<li>3x Solemn Vow (The Dream-Eaters)</li>\n<li>1x Versatile [2] (A Thousand Shapes of Horror)</li>\n<li>2x Safeguard [2] (Dark Side of the Moon)</li>\n</ul>\n<h3 id="events">Events<a class="anchor" href="#events">§</a></h3>\n<ul>\n<li>2x Dynamite Blast (Core Set)</li>\n<li>2x Stand Together [3] (The Essex County Express)</li>\n<li>1x Emergency Cache [2] (Blood on the Altar)</li>\n<li>1x Ambush [1] (The Unspeakable Oath)</li>\n<li>2x &quot;I\'ve had worse…&quot; [2] (Before the Black Throne)</li>\n<li>1x Astounding Revelation (The Dream-Eaters)</li>\n</ul>\n<h3 id="skills">Skills<a class="anchor" href="#skills">§</a></h3>\n<ul>\n<li>2x Deduction (Core Set)</li>\n<li>2x Guts (Core Set)</li>\n<li>2x Perception (Core Set)</li>\n</ul>\n<h3 id="treacheries">Treacheries<a class="anchor" href="#treacheries">§</a></h3>\n<ul>\n<li>1x Random Basic Weakness (Core Set)</li>\n<li>1x Rational Thought (The Circle Undone)</li>\n</ul>\n<h2 id="the-waking-world">The waking world：<a class="anchor" href="#the-waking-world">§</a></h2>\n<p><img src="https://arkhamdb.com/bundles/cards/06003.png" alt="Tony Morgan"></p>'
         } }),
     'head': null,
     'script': React.createElement(React.Fragment, null,
         React.createElement("script", { src: "https://cdn.pagic.org/react@16.13.1/umd/react.production.min.js" }),
         React.createElement("script", { src: "https://cdn.pagic.org/react-dom@16.13.1/umd/react-dom.production.min.js" }),
         React.createElement("script", { src: "/index.js", type: "module" })),
-    'contentTitle': React.createElement("h1", { key: "0" }, "Hello world effewefw"),
+    'contentTitle': React.createElement("h1", { key: "0" }, "The Dream-Eaters"),
     'contentBody': React.createElement("article", { dangerouslySetInnerHTML: {
-            __html: '<p>123</p>'
+            __html: '<h2 id="dreamlands">Dreamlands：<a class="anchor" href="#dreamlands">§</a></h2>\n<p><img src="https://arkhamdb.com/bundles/cards/05001.png" alt="Carolyn Fern"></p>\n<p>Packs: From Core Set (2) to Before the Black Throne</p>\n<h3 id="assets">Assets<a class="anchor" href="#assets">§</a></h3>\n<ul>\n<li>2x Clarity of Mind [3] (Return to the Dunwich Legacy)</li>\n<li>1x Scavenging (Core Set)</li>\n<li>2x Liquid Courage (The Dunwich Legacy)</li>\n<li>2x Peter Sylvestre:Big Man on Campus [2] (The Dunwich Legacy)</li>\n<li>1x Stick to the Plan [3] (Black Stars Rise)</li>\n<li>2x St. Hubert\'s Key:Cleansing Fire (Black Stars Rise)</li>\n<li>1x Hypnotic Therapy (The Circle Undone)</li>\n<li>2x Fingerprint Kit (The Circle Undone)</li>\n<li>1x Four of Cups:Chalice of the Heart [1] (The Circle Undone)</li>\n<li>2x Hawk-Eye Folding Camera (The Wages of Sin)</li>\n<li>1x Hallowed Mirror (Before the Black Throne)</li>\n<li>3x Solemn Vow (The Dream-Eaters)</li>\n<li>1x Versatile [2] (A Thousand Shapes of Horror)</li>\n<li>2x Safeguard [2] (Dark Side of the Moon)</li>\n</ul>\n<h3 id="events">Events<a class="anchor" href="#events">§</a></h3>\n<ul>\n<li>2x Dynamite Blast (Core Set)</li>\n<li>2x Stand Together [3] (The Essex County Express)</li>\n<li>1x Emergency Cache [2] (Blood on the Altar)</li>\n<li>1x Ambush [1] (The Unspeakable Oath)</li>\n<li>2x &quot;I\'ve had worse…&quot; [2] (Before the Black Throne)</li>\n<li>1x Astounding Revelation (The Dream-Eaters)</li>\n</ul>\n<h3 id="skills">Skills<a class="anchor" href="#skills">§</a></h3>\n<ul>\n<li>2x Deduction (Core Set)</li>\n<li>2x Guts (Core Set)</li>\n<li>2x Perception (Core Set)</li>\n</ul>\n<h3 id="treacheries">Treacheries<a class="anchor" href="#treacheries">§</a></h3>\n<ul>\n<li>1x Random Basic Weakness (Core Set)</li>\n<li>1x Rational Thought (The Circle Undone)</li>\n</ul>\n<h2 id="the-waking-world">The waking world：<a class="anchor" href="#the-waking-world">§</a></h2>\n<p><img src="https://arkhamdb.com/bundles/cards/06003.png" alt="Tony Morgan"></p>'
         } }),
-    'toc': null,
+    'toc': React.createElement("nav", { key: "0", className: "toc" },
+        React.createElement("ol", null,
+            React.createElement("li", null,
+                React.createElement("a", { href: "#dreamlands" }, "Dreamlands\uFF1A"),
+                React.createElement("ol", null,
+                    React.createElement("li", null,
+                        React.createElement("a", { href: "#assets" }, "Assets")),
+                    React.createElement("li", null,
+                        React.createElement("a", { href: "#events" }, "Events")),
+                    React.createElement("li", null,
+                        React.createElement("a", { href: "#skills" }, "Skills")),
+                    React.createElement("li", null,
+                        React.createElement("a", { href: "#treacheries" }, "Treacheries")))),
+            React.createElement("li", null,
+                React.createElement("a", { href: "#the-waking-world" }, "The waking world\uFF1A")))),
     'author': undefined,
     'contributors': [],
-    'date': "2021-04-07T02:35:14.188Z",
+    'date': "2021-04-07T06:03:33.369Z",
     'updated': null,
-    'excerpt': "123",
-    'cover': undefined
+    'excerpt': "Dreamlands： Packs: From Core Set (2) to Before the Black Throne Assets - 2x Clarity of Mind [3] (Return to the Dunwich Legacy) - 1x Scavenging (Core Set) - 2x Liquid Courage (The Dunwich Legacy) - 2x Peter Syl...",
+    'cover': "https://arkhamdb.com/bundles/cards/05001.png"
 };
